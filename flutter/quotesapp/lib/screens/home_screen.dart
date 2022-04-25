941dbddfc8900e1c27f83d7a35497d19175c850f
chinweibegbu <chinwe.ibegbu@gmail.com>
2022-04-25 20:32:44 +0000

    Created home screen

---
+++ flutter/quotesapp/lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text("Go to next Screen"),
+          ),
+        ),
+      ),
+    );
+  }
+}

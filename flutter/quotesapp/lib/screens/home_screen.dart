bd1fa972159da50e554995aa90f5faccb7379477
chinweibegbu <chinwe.ibegbu@gmail.com>
2022-04-26 13:49:05 +0000

    Utilised an API to fetch and parse JSON data

--- flutter/quotesapp/lib/screens/home_screen.dart
+++ flutter/quotesapp/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quotesapp/screens/second_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,11 +12,27 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: Center(
           child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SecondScreen()));
+            onPressed: () async {
+              // Use an HTTP GET request
+              var url = Uri.parse(
+                  'https://evilinsult.com/generate_insult.php?lang=en&type=json');
+              var response = await http.get(url);
+              print('Response status: ${response.statusCode}');
+              print('Response body: ${response.body}');
+
+              // Parse the JSON data
+              var data = jsonDecode(response.body);
+              String insult = (data['insult']);
+              String author = (data['createdby']);
+
+              // Navigate to the SecondScreen dart file
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SecondScreen(
+                        insultText: insult,
+                        authorName: author,
+                      )));
             },
-            child: Text("Go to next Screen"),
+            child: Text("Get Insult"),
           ),
         ),
       ),
--- flutter/quotesapp/lib/screens/second_screen.dart
+++ flutter/quotesapp/lib/screens/second_screen.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+  String insultText;
+  String authorName;
+
+  SecondScreen({Key? key, required this.insultText, required this.authorName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: Text("Go Back"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
-      ),
+          appBar: AppBar(),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  insultText,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 2.0),
+                child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Text("- John Doe")),
+              )
+            ],
+          )),
     );
   }
 }
